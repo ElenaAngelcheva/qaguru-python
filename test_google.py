7e58386c17a6fd9885e0c4e4c122dbdd62e7bae2
Pureevil <elenapureevil@gmail.com>
2022-06-20 16:41:44 +0300

    Add test_first in test_google.py

--- test_google.py
+++ test_google.py
@@ -1,3 +1,10 @@
+from selene.support.shared import browser
+from selene import be, have
+
+
 def test_first():
-    pass
+    browser.open('https://yandex.ru/')
+    browser.element('#text').should(be.blank).type(
+        'Selene - User-oriented Web UI browser tests in Python').press_enter()
+    browser.element('#search-result').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
