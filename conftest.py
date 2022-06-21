c5be3b30b5c3096ee361ecd9d6514bf51b637ec5
Elena <elenapureevil@gmail.com>
2022-06-21 07:45:42 +0300

    Add in fixture scope

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selene import be, have
 
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session', autouse=True)
 def browser_size():
     browser.open('data:')
     browser.driver.set_window_size(width=1920, height=1080)
