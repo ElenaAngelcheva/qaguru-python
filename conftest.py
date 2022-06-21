b1822f32fb9b8f637cf46d6ec4f9289e45d5e41a
ElenaAngelcheva <46818166+ElenaAngelcheva@users.noreply.github.com>
2022-06-21 07:37:20 +0300

    Update conftest.py
    delete scope

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selene import be, have
 
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture()
 def browser_size():
     browser.open('data:')
     browser.driver.set_window_size(width=1920, height=1080)
