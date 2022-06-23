00f831ccf450bf4efeef92b4c8404116431a2c85
Elena <elenapureevil@gmail.com>
2022-06-23 08:49:04 +0300

    Add "if" in def description_of_actions

--- homework_4.py
+++ homework_4.py
@@ -9,14 +9,17 @@ def go_to_companyname_homepage(ozon, wildberries):
     pass
 
 
-def find_registration_button_on_login_page(a):
+def find_registration_button_on_login_page():
     pass
 
 
 def description_of_actions(func):
     arguments = str(inspect.signature(func)).replace("(", "").replace(")", "")
     description = func.__name__.replace("_", " ").capitalize(), arguments
-    print(*description, sep=': ')
+    if len(arguments) > 0:
+        print(*description, sep=': ')
+    else:
+        print(*description)
 
 
 functions = open_browser, go_to_companyname_homepage, find_registration_button_on_login_page
